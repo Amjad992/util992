a1a760493b8b16bcf28c2570893211bf003ca9e8
amjad992 <amjad.majed992@gmail.com>
2020-12-30 12:38:45 +0500

    Feat: add get table, base and baseRecordsId

--- functions/airtable/get.js
+++ functions/airtable/get.js
@@ -5,12 +5,160 @@ const dev = require('../dev');
 
 const generalFuncs = require('../general');
 
+/** Return a base with multiple tables from airtable
+ * @async
+ * @param {Array} tablesArr - An Array including the names of all the tables in the base
+ * @param {string} apiKey - The api key // (Optional - configurable through the config.airtable object) //
+ * @param {string} baseURL - The base url // (Optional - Default is 'https://api.airtable.com/v0/' - configurable through the config.airtable object) //
+ * @param {string} baseId - The base id // (Optional - configurable through the config.airtable object) //
+ * @returns - Return a response following this module's format (Created using func.constructResponse functionality)
+ ** The body will have the records array (Can be empty array [] if no records found and the request will still be success)
+ ** In case of error (error of execution e.g. no url provided, not url hit error response), it will throw an exception with an object following the same format, for the url hit error responses, they will be returned as success but full body will be in the body value
+ */
+module.exports.base = async (
+  tablesArr,
+  apiKey = v.airtable.apiKey,
+  baseURL = v.airtable.baseURL,
+  baseId = v.airtable.baseId
+) => {
+  try {
+    let resCode;
+    dev.throwErrorIfValueNotPassed(tablesArr, 'tablesArr');
+    let base = {};
+    for (table in tablesArr) {
+      tableName = tablesArr[table];
+      const tableRes = await this.table(
+        tableName,
+        undefined,
+        apiKey,
+        baseURL,
+        baseId
+      );
+      base[`${tableName}`] = tableRes.body;
+      resCode = tableRes.code;
+    }
+
+    return generalFuncs.constructResponse(
+      true,
+      resCode,
+      `Retrieved ${tablesArr} tables records from airtable`,
+      base
+    );
+  } catch (err) {
+    throw dev.formatError(err);
+  }
+};
+
+/** Return a record ids of base with multiple tables from airtable
+ * @async
+ * @param {Array} tablesArr - An Array including the names of all the tables in the base
+ * @param {string} apiKey - The api key // (Optional - configurable through the config.airtable object) //
+ * @param {string} baseURL - The base url // (Optional - Default is 'https://api.airtable.com/v0/' - configurable through the config.airtable object) //
+ * @param {string} baseId - The base id // (Optional - configurable through the config.airtable object) //
+ * @returns - Return a response following this module's format (Created using func.constructResponse functionality)
+ ** The body will have the records array (Can be empty array [] if no records found and the request will still be success)
+ ** In case of error (error of execution e.g. no url provided, not url hit error response), it will throw an exception with an object following the same format, for the url hit error responses, they will be returned as success but full body will be in the body value
+ */
+module.exports.baseRecoresIds = async (
+  tablesArr,
+  apiKey = v.airtable.apiKey,
+  baseURL = v.airtable.baseURL,
+  baseId = v.airtable.baseId
+) => {
+  try {
+    let resCode;
+    dev.throwErrorIfValueNotPassed(tablesArr, 'tablesArr');
+
+    const baseRes = await this.base(tablesArr, apiKey, baseURL, baseId);
+
+    let idsObj = {};
+    for (i in tablesArr) {
+      const tableName = tablesArr[i];
+      const records = baseRes.body[tableName];
+      idsObj[tableName] = [];
+
+      for (j in records) {
+        idsObj[tableName].push(records[j].id);
+      }
+    }
+
+    return generalFuncs.constructResponse(
+      true,
+      undefined,
+      `Retrieved ${tablesArr} tables records ids from airtable`,
+      idsObj
+    );
+  } catch (err) {
+    throw dev.formatError(err);
+  }
+};
+
+/** Return a table records from airtable
+ * @async
+ * @param {string} tableName - The table name
+ * @param {string} formula - The formula used to filter the records (This follows airtable format) // (Optional - Default undefined) //
+ * @param {string} apiKey - The api key // (Optional - configurable through the config.airtable object) //
+ * @param {string} baseURL - The base url // (Optional - Default is 'https://api.airtable.com/v0/' - configurable through the config.airtable object) //
+ * @param {string} baseId - The base id // (Optional - configurable through the config.airtable object) //
+ * @returns - Return a response following this module's format (Created using func.constructResponse functionality)
+ ** The body will have the records array (Can be empty array [] if no records found and the request will still be success)
+ ** In case of error (error of execution e.g. no url provided, not url hit error response), it will throw an exception with an object following the same format, for the url hit error responses, they will be returned as success but full body will be in the body value
+ */
+module.exports.table = async (
+  tableName,
+  formula = undefined,
+  apiKey = v.airtable.apiKey,
+  baseURL = v.airtable.baseURL,
+  baseId = v.airtable.baseId
+) => {
+  let iterationCounter = 1;
+
+  try {
+    let airtableResponse = await this.nRecords(
+      tableName,
+      undefined,
+      undefined,
+      formula,
+      apiKey,
+      baseURL,
+      baseId
+    );
+    offset = airtableResponse.offset;
+
+    while (offset) {
+      ++iterationCounter;
+
+      airtableSingleResponse = await this.nRecords(
+        tableName,
+        undefined,
+        offset,
+        formula,
+        apiKey,
+        baseURL,
+        baseId
+      );
+
+      offset = airtableSingleResponse.offset;
+
+      airtableResponse.body = [
+        ...airtableResponse.body,
+        ...airtableSingleResponse.body,
+      ];
+    }
+
+    airtableResponse.message = `Retrieved ${airtableResponse.body.length} records from airtable`;
+    return airtableResponse;
+  } catch (err) {
+    throw dev.formatError(err);
+  }
+};
+
 /** Return a number of records from a table
  * @async
  * @param {string} table - The table name
  * @param {number} numberOfRecords = The number of records to be retrieved // (Optional - Default value is 100) //
  * @param {string} offset - The offset string provided by airtable response on the previous get records process // (Optional - Default undefined) //
- * @param {string} formula - The formula used to filter the arrays (This follows airtable format) // (Optional - Default undefined) //
+ * @param {string} formula - The formula used to filter the records (This follows airtable format) // (Optional - Default undefined) //
  * @param {string} apiKey - The api key // (Optional - configurable through the config.airtable object) //
  * @param {string} baseURL - The base url // (Optional - Default is 'https://api.airtable.com/v0/' - configurable through the config.airtable object) //
  * @param {string} baseId - The base id // (Optional - configurable through the config.airtable object) //
@@ -40,7 +188,6 @@ module.exports.nRecords = async (
     if (offset) {
       url += `&offset=${offset}`;
     }
-    console.log(url);
 
     const response = await axios({
       method: 'get',
@@ -52,12 +199,13 @@ module.exports.nRecords = async (
 
     const resData = response.data;
     const records = resData.records;
+
     return generalFuncs.constructResponse(
       true,
       response.code,
       `Retrieved ${records.length} records from airtable`,
       records,
-      resData.offset
+      {offset: resData.offset}
     );
   } catch (err) {
     throw dev.formatError(err);
